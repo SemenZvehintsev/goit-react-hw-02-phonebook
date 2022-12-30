95d3f9a543de62a78d26920975dfbaec5eae974f
SemenZvehintsev <kvadratniibomzh@gmail.com>
2022-12-30 14:40:45 +0200

    styles fix

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import styles from 'components/ContactItem/ContactItem.module.css'
 
 export const ContactItem =({id, contactName, contactNumber, onClickDelete})=> {
-    return <li className={styles.contact}>{contactName}: {contactNumber} 
+    return <li className={styles.contact}> - {contactName}: {contactNumber} 
     <button className={styles.button} type="button" onClick={() => onClickDelete(id)}>Delete</button> 
     </li>
 }
